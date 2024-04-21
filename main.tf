0d2525f0b606307384f135b51e1f93cbe1cbcd11
Jamaligy <153540032+Jamaligy@users.noreply.github.com>
2024-04-21 12:46:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform_today" {
+resource "aws_s3_bucket" "terraform_jamal" {
   bucket = "project90" 
 }
 resource "aws_instance" "test5" {
