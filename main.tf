4bec41e23349753ec850d010666c27606e728181
Jamaligy <153540032+Jamaligy@users.noreply.github.com>
2024-04-21 12:52:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform_jamal" {
-  bucket = "project90" 
+resource "aws_s3_bucket" "sunday12" {
+  bucket = "sunday12" 
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "sunday12"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
 resource "aws_instance" "test5" {
   ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
